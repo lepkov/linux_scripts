25c0bcf0120cc84708c167f0344d9ce9c131595c
Andrew Lepkov <lepkov@users.noreply.github.com>
2019-11-26 10:47:29 +0500

    Update DNS_SOA_SCAN.sh

--- DNS_SOA_SCAN.sh
+++ DNS_SOA_SCAN.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 while [ -n "$1" ]
 do
-temp=`dig SOA $1 | sed -n '/ANSWER SECTION/{n;p;}'| awk {'print $5'}`
-echo "$temp"
+echo `dig SOA $1 | sed -n '/ANSWER SECTION/{n;p;}'| awk {'print $5'}`
 shift
 done
