dec086d4a223812230a5ced89935de11e3a6aab4
Andrew Lepkov <lepkov@users.noreply.github.com>
2022-02-03 18:00:06 +0500

    Update whois_bulk.sh

--- whois_bulk.sh
+++ whois_bulk.sh
@@ -5,10 +5,16 @@ echo "WHOIS for IPs in ip.txt:" > result.txt
 for ip in `cat $IP_LIST`
 do
     L1=`whois $ip | grep -m 1 'OrgName' | tr -s ' ' | sed 's/OrgName: //g'`
+    [[ "$L1" == "" ]] && L1=`whois $ip | grep -m 1 'org-name' | tr -s ' ' | sed 's/org-name: //g'`
     L2=`whois $ip | grep -m 1 'Country' | tr -s ' ' | sed 's/Country: //g'`
+    [[ "$L2" == "" ]] && L2=`whois $ip | grep -m 1 'country' | tr -s ' ' | sed 's/country: //g'`
     L3=`whois $ip | grep -m 1 'City' | tr -s ' ' | sed 's/City: //g'`
+    [[ "$L3" == "" ]] && L3="No City"
     L4=`whois $ip | grep -m 1 'StateProv' | tr -s ' ' | sed 's/StateProv: //g'`
-    C="${ip}, ${L1}, ${L2}, ${L3}, ${L4}"
+    [[ "$L4" == "" ]] && L4="No State"
+    C="${ip}; ${L1}; ${L2}; ${L3}; ${L4}"
     echo $C >> result.txt
     #sleep 1
 done
+
+#cat result.txt | awk -F "; " '{print $3}'
