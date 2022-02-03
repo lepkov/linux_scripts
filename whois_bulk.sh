113b03487845a952659e0d343c02fb0f35fcd169
Andrew Lepkov <lepkov@users.noreply.github.com>
2022-02-03 18:45:59 +0500

    Update whois_bulk.sh

--- whois_bulk.sh
+++ whois_bulk.sh
@@ -3,18 +3,19 @@
 IP_LIST="ip.txt"
 echo "WHOIS for IPs in ip.txt:" > result.txt
 for ip in `cat $IP_LIST`
-do
-    L1=`whois $ip | grep -m 1 'OrgName' | tr -s ' ' | sed 's/OrgName: //g'`
-    [[ "$L1" == "" ]] && L1=`whois $ip | grep -m 1 'org-name' | tr -s ' ' | sed 's/org-name: //g'`
-    L2=`whois $ip | grep -m 1 'Country' | tr -s ' ' | sed 's/Country: //g'`
-    [[ "$L2" == "" ]] && L2=`whois $ip | grep -m 1 'country' | tr -s ' ' | sed 's/country: //g'`
-    L3=`whois $ip | grep -m 1 'City' | tr -s ' ' | sed 's/City: //g'`
+do 
+    query=`whois $ip`
+    L1=`echo "$query" | grep -m 1 'OrgName' | tr -s ' ' | sed 's/OrgName: //g'`
+    [[ "$L1" == "" ]] && L1=`echo "$query" | grep -m 1 'org-name' | tr -s ' ' | sed 's/org-name: //g'`
+    L2=`echo "$query" | grep -m 1 'Country' | tr -s ' ' | sed 's/Country: //g'`
+    [[ "$L2" == "" ]] && L2=`echo "$query" | grep -m 1 'country' | tr -s ' ' | sed 's/country: //g'`
+    L3=`echo "$query" | grep -m 1 'City' | tr -s ' ' | sed 's/City: //g'`
     [[ "$L3" == "" ]] && L3="No City"
-    L4=`whois $ip | grep -m 1 'StateProv' | tr -s ' ' | sed 's/StateProv: //g'`
+    L4=`echo "$query" | grep -m 1 'StateProv' | tr -s ' ' | sed 's/StateProv: //g'`
     [[ "$L4" == "" ]] && L4="No State"
-    L5=`whois $ip | grep -m 1 'role' | tr -s ' ' | sed 's/role: //g'`
+    L5=`echo "$query" | grep -m 1 'role' | tr -s ' ' | sed 's/role: //g'`
     [[ "$L5" == "" ]] && L5="No Role"
-    L6=`whois $ip | grep -m 1 'netname' | tr -s ' ' | sed 's/netname: //g'`
+    L6=`echo "$query" | grep -m 1 'netname' | tr -s ' ' | sed 's/netname: //g'`
     [[ "$L6" == "" ]] && L6="No Netname"
     C="${ip}; ${L1}; ${L2}; ${L3}; ${L4}; ${L5}; ${L6}"
     echo $C >> result.txt
